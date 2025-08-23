27e1affb2c7742fa03081dbee85a1f10c59c7e32
Lipe <luizhidalgo00@gmail.com>
2025-08-23 03:01:19 -0300

    Corrigir React Router para GitHub Pages usando HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Portfolio from './components/Portfolio'
 import Admin from './components/Admin'
 import { AuthProvider } from './contexts/AuthContext'
